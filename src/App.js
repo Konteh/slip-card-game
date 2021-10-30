8ee2c2001a6bb0e1ec813acd3c628fa30e576336
Konteh <kontehebrima993@gmail.com>
2021-10-30 11:37:42 -0700

    add card choice

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   //sheffule card
   const shuffleCard = () => {
@@ -25,7 +27,13 @@ function App() {
     setTurns(0);
 
   }
-  console.log(cards, turns);
+  
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card) 
+  }
+
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -33,7 +41,11 @@ function App() {
 
       <div className="card-grid">
         {cards.map(card=>(
-         <SingleCard key={card.id} card={card}/>
+         <SingleCard 
+          key={card.id} 
+          card={card}
+          handleChoice={handleChoice}
+         />
         ))}
       </div>
     </div>
